1d25201002c080951e149c5ec5dd1013b1e2e4eb
pete4nhs <165719339+pete4nhs@users.noreply.github.com>
2026-05-12 22:04:29 +0100

    Refactor test.py to include greeting and imports
    Reorganized import statements and added a greeting message.

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 # cd "C:\Users\peter.saiu\OneDrive - NHS\Scripts\Python\Automating Local Prices checks\"
 
 import streamlit as st
-#st.write("Hello from entrypoint")
+st.write("Hello from entrypoint")
 
-from IAP_Checks import run_app
-
-run_app()
