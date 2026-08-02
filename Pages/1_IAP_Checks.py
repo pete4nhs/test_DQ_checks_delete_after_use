20ca08d9a16dba78f672e35cf9dd59eb4597e5cc
pete4nhs <165719339+pete4nhs@users.noreply.github.com>
2026-08-02 21:59:24 +0100

    Update 1_IAP_Checks.py

--- Pages/1_IAP_Checks.py
+++ Pages/1_IAP_Checks.py
@@ -709,28 +709,36 @@ def validate_specialised_mental_health_code_columns(df):
 
 # --------------------- POINT OF DELIVERY CODE (mandatory)
 def validate_pod_code_columns(df):
+
     col = 'POINT OF DELIVERY CODE'
+
     if col not in df.columns:
         return f"Error: '{col}' column not found in the data."
 
     # when running locally
     # npod = pd.read_csv(
-    #     r"C:\Users\peter.saiu\OneDrive - NHS\Scripts\Python\Automating_IAPs_&_Local_Prices_DQ_checks\reference_tables\NPOD.csv")
+    #     r"C:\Users\peter.saiu\OneDrive - NHS\Scripts\Python\Automating_IAPs_&_Local_Prices_DQ_checks\reference_tables\NPOD.csv"
+    # )
 
     # when running in stlite
-    NPOD_URL = ("https://raw.githubusercontent.com/pete4nhs/DQ_checks/main/reference_tables/NPOD.csv")
+    NPOD_URL = (
+        "https://raw.githubusercontent.com/pete4nhs/"
+        "DQ_checks/main/reference_tables/NPOD.csv"
+    )
     npod = pd.read_csv(NPOD_URL)
 
     # Clean and normalise NPOD reference values
     valid_codes = set(
         clean_numeric_text(npod.iloc[:, 0])
         .str.upper()
-        .dropna())
+        .dropna()
+    )
 
     # Clean and normalise uploaded POD values
     pod = (
         clean_numeric_text(df[col])
-        .str.upper())
+        .str.upper()
+    )
 
     # Diagnostic output
     invalid_mask = ~pod.isin(valid_codes)
